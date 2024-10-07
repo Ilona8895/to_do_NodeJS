a3385d29a6921a54296a2b0954e2ff909cfac162
Ilona Melcher <ilona.melcher8@gmail.com>
2024-10-07 14:30:38 +0200

    update

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ app.patch("/todo/:todoId", (req, res) => {
   } else res.status(404).end();
 });
 
-// const port = prosess.env.PORT || 5000;
+const port = process.env.PORT || 8888;
 
-app.listen(8888, () => {
-  console.log(`Start aplikacji na porcie 8888`);
+app.listen(port, () => {
+  console.log(`Start aplikacji na porcie ${port}`);
 });
